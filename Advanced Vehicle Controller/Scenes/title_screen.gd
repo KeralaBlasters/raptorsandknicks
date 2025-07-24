90c128c38a4a06cd34b599a3b5697d8953cbb7d7
KeralaBlasters <isatho25@kingshigh.school.nz>
2025-07-25 11:00:01 +1200

    Updated

---
+++ Advanced Vehicle Controller/Scenes/title_screen.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://Advanced Vehicle Controller/Scenes/test_world.tscn")
+
+
+func _on_options_button_pressed():
+	get_tree().change_scene_to_file("res://Advanced Vehicle Controller/Scenes/options_menu.tscn")
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
