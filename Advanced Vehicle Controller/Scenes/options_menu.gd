d03cbb4db26f73d365b7fd9605bc2512483c3e88
KeralaBlasters <isatho25@kingshigh.school.nz>
2025-07-29 13:45:56 +1200

    Worked on the title screen
    Worked on finalising the title screen and main menus

--- Advanced Vehicle Controller/Scenes/options_menu.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-
-
-func _on_back_button_pressed():
-	get_tree().change_scene_to_file("res://Advanced Vehicle Controller/Scenes/title_screen.tscn")
--- Advanced Vehicle Controller/Scenes/title_screen.gd
+++ Advanced Vehicle Controller/Scenes/title_screen.gd
@@ -1,14 +1,43 @@
 extends Control
 
-
+func _ready() -> void:
+	$MarginContainer/OptionsMenu/MainVolSlider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master")))
+	$MarginContainer/OptionsMenu/MusicVolSlider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("MUSIC")))
+	$MarginContainer/OptionsMenu/SFXVolSlider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("SFX")))
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://Advanced Vehicle Controller/Scenes/test_world.tscn")
 
 
 func _on_options_button_pressed():
-	get_tree().change_scene_to_file("res://Advanced Vehicle Controller/Scenes/options_menu.tscn")
+	$MarginContainer/MainButtons.visible = false
+	$MarginContainer/OptionsMenu.visible = true
 
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_back_pressed() -> void:
+	$MarginContainer/OptionsMenu.visible = false
+	$MarginContainer/MainButtons.visible = true
+	
+
+
+func _on_full_screen_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
+
+
+func _on_main_vol_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Master"), value)
+
+
+func _on_music_vol_slider_2_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("MUSIC"), value)
+
+
+func _on_sfx_vol_slider_3_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("SFX"), value)
