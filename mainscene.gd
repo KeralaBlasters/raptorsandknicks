f33d5d772b28f24f5d3c84951dc17e969433d534
KeralaBlasters <isatho25@kingshigh.school.nz>
2025-10-22 13:12:20 +1300

    Main more progress on the races and added an end screen

--- mainscene.gd
+++ mainscene.gd
@@ -22,7 +22,7 @@ var intro_checkpoint_number = -1000
 var second_checkpoint_number = -1000
 var third_checkpoint_number = -1000
 
-var intro_ai_chekcpoint_number = -1000
+var intro_ai_checkpoint_number = -1000
 var second_ai_checkpoint_number = -1000
 var third_ai_checkpoint_number = -1000
 
@@ -34,7 +34,11 @@ func _ready() -> void:
 	randomize()
 	play_random_track()
 	$Races/IntroRace/Arrows.hide()
-	
+	var delete = $Delete
+	var second_enter_area = $Races/SecondRace/SecondRaceStartArea
+	var third_enter_area = $Races/ThirdRace/ThirdRaceStartArea
+	second_enter_area.global_position = delete.global_position
+	third_enter_area.global_position = delete.global_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -65,8 +69,20 @@ func _process(delta: float) -> void:
 	if intro_checkpoint_number == 9:
 		finish_intro_race()
 	
+	if intro_ai_checkpoint_number == 9:
+		ai_finish_intro_race()
+	
 	if second_checkpoint_number == 14:
 		finish_second_race()
+	
+	if second_ai_checkpoint_number == 14:
+		ai_finish_second_race()
+	
+	if third_checkpoint_number == 10:
+		finish_third_race()
+	
+	if third_ai_checkpoint_number == 10:
+		ai_finish_third_race()
 
 
 var music_tracks = [
@@ -129,6 +145,7 @@ func _on_enter_race_area_body_exited(body: Node3D) -> void:
 
 
 func start_intro_race():
+	intro_ai_checkpoint_number = 0
 	intro_checkpoint_number = 0
 	var player_intro_spawn = $Races/IntroRace/PlayerIntroSpawn
 	var player = $"Muscle Car"
@@ -188,22 +205,33 @@ func _on_enter_intro_race_area_body_exited(body: Node3D) -> void:
 func _on_first_checkpoint_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		intro_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car":
+		intro_ai_checkpoint_number +=1
 
 
 func _on_second_checkpoint_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		intro_checkpoint_number += 1
 		
+	if body.name == "AI_Muscle_Car":
+		intro_ai_checkpoint_number +=1
 
 
 func _on_third_checkpoint_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		intro_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car":
+		intro_ai_checkpoint_number +=1
 
 
 func _on_fourth_checkpoint_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		intro_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car":
+		intro_ai_checkpoint_number +=1
 
 
 
@@ -212,6 +240,8 @@ func _on_fifth_checkpoint_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		intro_checkpoint_number += 1
 		
+	if body.name == "AI_Muscle_Car":
+		intro_ai_checkpoint_number +=1
 
 
 
@@ -220,12 +250,17 @@ func _on_sixth_checkpoint_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		intro_checkpoint_number += 1
 		
+	if body.name == "AI_Muscle_Car":
+		intro_ai_checkpoint_number +=1
 
 
 
 func _on_seventh_checkpoint_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		intro_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car":
+		intro_ai_checkpoint_number +=1
 
 
 
@@ -234,11 +269,16 @@ func _on_eighth_checkpoint_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		intro_checkpoint_number += 1
 		
+	if body.name == "AI_Muscle_Car":
+		intro_ai_checkpoint_number +=1
 
 
 func _on_finish_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		intro_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car":
+		intro_ai_checkpoint_number +=1
 
 
 func finish_intro_race():
@@ -248,14 +288,29 @@ func finish_intro_race():
 	await get_tree().create_timer(3.0).timeout
 	text.text = ""
 	var delete = $Delete
+	var intro_race = $Races/IntroRace
+	intro_race.global_position = delete.global_position
 	#ai_intro_racer.global_position = delete.global_position
 	#$Races/SecondRace/SecondRaceCheckpoints.queue_free()
 	await get_tree().create_timer(10.0).timeout
 	$Races/SecondRace.show()
 	$Races/SecondRace/SecondArrows.hide()
 	intro_checkpoint_number = -1000
+	var second_start_area_pos = $Races/SecondRace/SecondStartAreaPosition
+	var second_start_area = $Races/SecondRace/SecondRaceStartArea
+	second_start_area.global_position = second_start_area_pos.global_position
+	
 	
 
+func ai_finish_intro_race():
+	intro_checkpoint_number = -1000
+	text.text = "YOU LOST"
+	await get_tree().create_timer(3.0).timeout
+	text.text = ""
+	$Races/IntroRace/Arrows.hide()
+	await get_tree().create_timer(20.0).timeout
+	$Races/IntroRace/start_arrow.show()
+	text.text = "LET'S GO AROUND THE BLOCK AGAIN"
 
 
 func _on_second_race_start_area_body_entered(body: Node3D) -> void:
@@ -278,6 +333,7 @@ func _on_second_race_start_area_body_exited(body: Node3D) -> void:
 
 
 func start_second_race():
+	second_ai_checkpoint_number = 0
 	second_checkpoint_number = 0
 	$Races/SecondRace/second_start_arrow.hide()
 	$Races/SecondRace/SecondArrows.show()
@@ -313,76 +369,136 @@ func start_second_race():
 func _on_checkpoint_1_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		second_checkpoint_number += 1
-
+		
+	if body.name == "AI_Muscle_Car2":
+		second_ai_checkpoint_number += 1
 
 func _on_checkpoint_2_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		second_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car2":
+		second_ai_checkpoint_number += 1
 
 
 func _on_checkpoint_3_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		second_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car2":
+		second_ai_checkpoint_number += 1
 
 
 func _on_checkpoint_4_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		second_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car2":
+		second_ai_checkpoint_number += 1
 
 
 func _on_checkpoint_5_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		second_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car2":
+		second_ai_checkpoint_number += 1
 
 
 func _on_checkpoint_6_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		second_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car2":
+		second_ai_checkpoint_number += 1
 
 
 func _on_checkpoint_7_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		second_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car2":
+		second_ai_checkpoint_number += 1
 
 
 func _on_checkpoint_8_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		second_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car2":
+		second_ai_checkpoint_number += 1
 
 
 func _on_checkpoint_9_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		second_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car2":
+		second_ai_checkpoint_number += 1
 
 
 func _on_checkpoint_10_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		second_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car2":
+		second_ai_checkpoint_number += 1
 
 
 func _on_checkpoint_11_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		second_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car2":
+		second_ai_checkpoint_number += 1
 
 
 func _on_checkpoint_12_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		second_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car2":
+		second_ai_checkpoint_number += 1
 
 
 func _on_checkpoint_13_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		second_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car2":
+		second_ai_checkpoint_number += 1
 
 
 func _on_checkpoint_14_body_entered(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		second_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car2":
+		second_ai_checkpoint_number +=1
 
 func finish_second_race():
 	text.text = "FINISH"
+	await get_tree().create_timer(3.0).timeout
+	text.text = ""
+	var delete = $Delete
+	$Races/ThirdRace.show()
+	$Races/ThirdRace/ThirdArrows.hide()
+	var third_start_area_pos = $Races/ThirdRace/ThirdStartAreaPosition
+	var third_start_area = $Races/ThirdRace/ThirdRaceStartArea
+	third_start_area.global_position = third_start_area_pos.global_position
+	var second_race = $Races/SecondRace
+	second_race.hide()
+	second_race.global_position = delete.global_position
 
- 
+func ai_finish_second_race():
+	second_checkpoint_number = -1000
+	text.text = "YOU LOST"
+	await get_tree().create_timer(3.0).timeout
+	text.text = ""
+	$Races/SecondRace/SecondArrows.hide()
+	await get_tree().create_timer(20.0).timeout
+	$Races/IntroRace/start_arrow.show()
+	text.text = "THE RACER WANTS A REMATCH"
 
 
 func _on_third_race_start_area_body_entered(body: Node3D) -> void:
@@ -398,12 +514,13 @@ func _on_third_race_start_area_body_entered(body: Node3D) -> void:
 func _on_third_race_start_area_body_exited(body: Node3D) -> void:
 	if body.name == "Muscle Car":
 		text.text = ""
-		can_start_second_race = false
+		can_start_third_race = false
 	else:
 		pass
 
 
 func start_third_race():
+	third_ai_checkpoint_number = 0
 	third_checkpoint_number = 0
 	$Races/ThirdRace/third_start_arrow.hide()
 	$Races/ThirdRace/ThirdArrows.show()
@@ -432,3 +549,107 @@ func start_third_race():
 	text.text = ""
 	third_race_follow.active = true
 	third_ai_car.active = true
+
+
+func _on_third_checkpoint_1_body_entered(body: Node3D) -> void:
+	if body.name == "Muscle Car":
+		third_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car3":
+		third_ai_checkpoint_number +=1
+
+
+
+func _on_third_checkpoint_2_body_entered(body: Node3D) -> void:
+	if body.name == "Muscle Car":
+		third_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car3":
+		third_ai_checkpoint_number +=1
+
+
+
+func _on_third_checkpoint_3_body_entered(body: Node3D) -> void:
+	if body.name == "Muscle Car":
+		third_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car3":
+		third_ai_checkpoint_number +=1
+
+
+func _on_third_checkpoint_4_body_entered(body: Node3D) -> void:
+	if body.name == "Muscle Car":
+		third_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car3":
+		third_ai_checkpoint_number +=1
+
+func _on_third_checkpoint_5_body_entered(body: Node3D) -> void:
+	if body.name == "Muscle Car":
+		third_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car3":
+		third_ai_checkpoint_number +=1
+
+
+func _on_third_checkpoint_6_body_entered(body: Node3D) -> void:
+	if body.name == "Muscle Car":
+		third_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car3":
+		third_ai_checkpoint_number +=1
+
+func _on_third_checkpoint_7_body_entered(body: Node3D) -> void:
+	if body.name == "Muscle Car":
+		third_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car3":
+		third_ai_checkpoint_number +=1
+
+
+func _on_third_checkpoint_8_body_entered(body: Node3D) -> void:
+	if body.name == "Muscle Car":
+		third_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car3":
+		third_ai_checkpoint_number +=1
+
+func _on_third_checkpoint_9_body_entered(body: Node3D) -> void:
+	if body.name == "Muscle Car":
+		third_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car3":
+		third_ai_checkpoint_number +=1
+
+func _on_third_checkpoint_10_body_entered(body: Node3D) -> void:
+	if body.name == "Muscle Car":
+		third_checkpoint_number += 1
+		
+	if body.name == "AI_Muscle_Car3":
+		third_ai_checkpoint_number +=1
+
+func finish_third_race():
+	text.text = "FINISH"
+	await get_tree().create_timer(3.0).timeout
+	text.text = ""
+	var delete = $Delete
+	$Races/ThirdRace.show()
+	$Races/ThirdRace/ThirdArrows.hide()
+	var third_start_area_pos = $Races/ThirdRace/ThirdStartAreaPosition
+	var third_start_area = $Races/ThirdRace/ThirdRaceStartArea
+	third_start_area.global_position = third_start_area_pos.global_position
+	await get_tree().create_timer(10.0).timeout
+	text.text = "CONGRATULATIONS ON BECOMING THE BEST RACER IN THE CITY"
+	await get_tree().create_timer(10.0).timeout
+	text.text = ""
+	get_tree().change_scene_to_file("res://end_screen.tscn")
+
+func ai_finish_third_race():
+	third_checkpoint_number = -1000
+	text.text = "YOU LOST"
+	await get_tree().create_timer(3.0).timeout
+	text.text = ""
+	$Races/ThirdRace/ThirdArrows.hide()
+	await get_tree().create_timer(20.0).timeout
+	$Races/ThirdRace/third_start_arrow.show()
+	text.text = "THE RACER WANTS A REMATCH"
